80a1c99fac7799bf0c4a28515bc8424cb28537e1
Blanc <pedro6893@gmail.com>
2018-03-14 10:23:15 -0300

    Update readme.md

--- public/scripts.js
+++ public/scripts.js
@@ -4,51 +4,6 @@ const newOp     = document.getElementById('newOp');
 const taskInfo  = document.getElementById('taskInfo');
 const taskTable = document.getElementById('taskTable');
 
-var request = new XMLHttpRequest();
-
-// const logo = document.createElement('img');
-// logo.src = 'logo.png';
-//
-// const container = document.createElement('div');
-// container.setAttribute('class', 'container');
-//
-// app.appendChild(logo);
-// app.appendChild(container);
-
-
-
-request.open('GET', 'htts://localhost:3000/tasks', true);
-request.onload = function () {
-
-  // // Begin accessing JSON data here
-  // var data = JSON.parse(this.response);
-  // if (request.status >= 200 && request.status < 400) {
-  //   data.forEach(movie => {
-  //     const card = document.createElement('div');
-  //     card.setAttribute('class', 'card');
-  //
-  //     const h1 = document.createElement('h1');
-  //     h1.textContent = movie.title;
-  //
-  //     const p = document.createElement('p');
-  //     movie.description = movie.description.substring(0, 300);
-  //     p.textContent = `${movie.description}...`;
-  //
-  //     container.appendChild(card);
-  //     card.appendChild(h1);
-  //     card.appendChild(p);
-  //   });
-  // } else {
-  //   const errorMessage = document.createElement('marquee');
-  //   errorMessage.textContent = `Gah, it's not working!`;
-  //   app.appendChild(errorMessage);
-  // }
-}
-
-// request.send();
-
-
-
 $(document).ready( function(){
     var reqTasks = new XMLHttpRequest();
     var reqSum = new XMLHttpRequest();
